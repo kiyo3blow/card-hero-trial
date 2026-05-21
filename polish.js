18561fdb4a849bef431e1aefebaacb5086eb3035
kiyo3blow <sansk9@gmail.com>
2026-05-21 14:55:11 +0900

    Use original monster SVG artwork

--- polish.js
+++ polish.js
@@ -1,4 +1,5 @@
 enemyList[0].spriteType = "slime";
+enemyList[1].spriteType = "goblin";
 enemyList[2].spriteType = "orc";
 enemyList[3].spriteType = "dragon";
 
@@ -8,7 +9,7 @@ function renderEnemySprite() {
   const spriteType = state.enemy.spriteType || "slime";
   els.enemySprite.className = `sprite enemy-sprite ${spriteType}`;
   els.enemySprite.innerHTML = `
-    <img class="monster-art" src="assets/monsters/${spriteType}.png" alt="">
+    <img class="monster-art" src="assets/monsters/${spriteType}.svg" alt="">
     <span class="ground-shadow"></span>
   `;
 }
